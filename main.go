e8d193ba4142ebf6401c6fe9a2182aa31e264e9e
Cesar David Clavijo Paez <Cclavijo@intergrupo.com>
2020-10-05 13:46:35 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+)
+
+func getService() (*drive.Service, error) {
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		fmt.Printf("Unable to read credentials.json file. Err: %v\n", err)
+		return nil, err
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := getClient(config)
+
+	service, err := drive.New(client)
+
+	if err != nil {
+		fmt.Printf("Cannot create the Google Drive service: %v\n", err)
+		return nil, err
+	}
+
+	return service, err
+}
+
+func createDir(service *drive.Service, name string, parentId string) (*drive.File, error) {
+	d := &drive.File{
+		Name:     name,
+		MimeType: "application/vnd.google-apps.folder",
+		Parents:  []string{parentId},
+	}
+
+	file, err := service.Files.Create(d).Do()
+
+	if err != nil {
+		log.Println("Could not create dir: " + err.Error())
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func createFile(service *drive.Service, name string, mimeType string, content io.Reader, parentId string) (*drive.File, error) {
+	f := &drive.File{
+		MimeType: mimeType,
+		Name:     name,
+		Parents:  []string{parentId},
+	}
+	file, err := service.Files.Create(f).Media(content).Do()
+
+	if err != nil {
+		log.Println("Could not create file: " + err.Error())
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func main() {
+
+	var argu string = os.Args[1]
+	var argu2 string = os.Args[2]
+
+
+	f, err := os.Open(argu)
+
+	if err != nil {
+		panic(fmt.Sprintf("cannot open file: %v", err))
+	}
+
+	defer f.Close()
+
+	service, err := getService()
+
+	dir, err := createDir(service, argu2, "root")
+
+	if err != nil {
+		panic(fmt.Sprintf("Could not create dir: %v\n", err))
+	}
+
+	file, err := createFile(service, argu, "application/x-7z-compressed", f, dir.Id)
+
+	if err != nil {
+		panic(fmt.Sprintf("Could not create file: %v\n", err))
+	}
+
+	fmt.Printf("File '%s' successfully uploaded in '%s' directory", file.Name,  dir.Name)
+
+}
